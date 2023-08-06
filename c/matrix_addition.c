8b02b1d269dae4d21c22c66471ff6799a09abad8
santhosh-cs21 <santhosht.cs21@kascsathy.ac.in>
2023-08-06 18:51:37 +0000

    2user

--- c/matrix_addition.c
+++ c/matrix_addition.c
@@ -1,41 +1,33 @@
 #include <stdio.h>
-int main() {
-  int r, c, a[100][100], b[100][100], sum[100][100], i, j;
-  printf("Enter the number of rows (between 1 and 100): ");
-  scanf("%d", &r);
-  printf("Enter the number of columns (between 1 and 100): ");
-  scanf("%d", &c);
+int main()
+    {
+    	int a[50][50],b[50][50],sum[50][50],i,j,r,c;
+	printf("ENTER THE ROWS :\n ");
+	scanf("%d",&r);
+	printf("ENTER THE COLUMNS :\n ");
+	scanf("%d",&c);
+    	printf("\nENTER VALUES FOR MATRIX A %d * %d :\n",r,c);
+    	for(i=0;i<r;i++) {
+    		for(j=0;j<c;j++)
+    			scanf("%d",&a[i][j]);
+	}
 
-  printf("\nEnter elements of 1st matrix:\n");
-  for (i = 0; i < r; ++i)
-    for (j = 0; j < c; ++j) {
-      printf("Enter element a%d%d: ", i + 1, j + 1);
-      scanf("%d", &a[i][j]);
+    	printf("\nENTER VALUES FOR MATRIX B %d * %d :\n",r,c);
+    	for(i=0;i<r;i++) {
+    		for(j=0;j<c;j++)
+    			scanf("%d",&b[i][j]);
+	}
+
+    	for(i=0;i<r;i++) {
+    		for(j=0;j<c;j++)
+    			sum[i][j]=a[i][j]+b[i][j];
+	}
+
+    	printf("\nTHE ADDITION OF MATRIX ARE %d * %d :\n",r,c);
+    	for(i=0;i<r;i++) {
+    		for(j=0;j<c;j++)
+    			printf("%d   ",sum[i][j]);
+    		printf("\n");
+    	}
+    	return 0;
     }
-
-  printf("Enter elements of 2nd matrix:\n");
-  for (i = 0; i < r; ++i)
-    for (j = 0; j < c; ++j) {
-      printf("Enter element b%d%d: ", i + 1, j + 1);
-      scanf("%d", &b[i][j]);
-    }
-
-  // adding two matrices
-  for (i = 0; i < r; ++i)
-    for (j = 0; j < c; ++j) {
-      sum[i][j] = a[i][j] + b[i][j];
-    }
-
-  // printing the result
-  printf("\nSum of two matrices: \n");
-  for (i = 0; i < r; ++i)
-    for (j = 0; j < c; ++j) {
-      printf("%d   ", sum[i][j]);
-      if (j == c - 1) {
-        printf("\n\n");
-      }
-    }
-
-  return 0;
-}
-
